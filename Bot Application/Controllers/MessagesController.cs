b9d7a3129afaf5f05d6eff129df35d38a10cceb5
Kiang Kuang <kiangkuang@gmail.com>
2017-02-20 02:53:18 +0800

    opencv get contour

--- Bot Application/Controllers/MessagesController.cs
+++ Bot Application/Controllers/MessagesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -9,6 +10,8 @@ using System.Threading.Tasks;
 using System.Web.Hosting;
 using System.Web.Http;
 using Microsoft.Bot.Connector;
+using OpenCvSharp;
+using OpenCvSharp.CPlusPlus;
 using Tesseract;
 
 namespace Bot_Application.Controllers
@@ -52,12 +55,12 @@ namespace Bot_Application.Controllers
             }
             catch (Exception e)
             {
-                System.Diagnostics.Debug.WriteLine(e);
+                Debug.WriteLine(e);
                 await Reply(activity, e.ToString());
+                return Request.CreateResponse(HttpStatusCode.InternalServerError);
             }
 
-            var response = Request.CreateResponse(HttpStatusCode.OK);
-            return response;
+            return Request.CreateResponse(HttpStatusCode.OK);
         }
 
         private async Task ProcessMessage(Activity activity)
@@ -66,7 +69,10 @@ namespace Bot_Application.Controllers
             {
                 foreach (var attachment in activity.Attachments)
                 {
-                    await Reply(activity, string.Join("\n", Ocr(attachment.ContentUrl).Select(x => string.Join(" ", x)).ToList()));
+                    var file = DownloadFile(attachment.ContentUrl);
+                    ProcessImage(file);
+                    var results = Ocr(file);
+                    await Reply(activity, string.Join("\n", results.Select(x => string.Join(" ", x)).ToList()));
                 }
             }
             else
@@ -75,17 +81,66 @@ namespace Bot_Application.Controllers
             }
         }
 
+        private void ProcessImage(string file)
+        {
+            var orig = Cv2.ImRead(file);
+            var ratio = 800.0 / orig.Height;
+            var blur = new Mat();
+            var edged = new Mat();
+            var image = orig.Clone();
+
+            image = image.Resize(new Size(), ratio, ratio);
+            Cv2.GaussianBlur(image, blur, new Size(5, 5), 0);
+            Cv2.Canny(blur, edged, 75, 200);
+
+            Point[][] contours;
+            HierarchyIndex[] hierarchyIndexes;
+            Cv2.FindContours(edged.Clone(), out contours, out hierarchyIndexes, ContourRetrieval.List, ContourChain.ApproxSimple);
+            var sortedContours = contours.OrderByDescending(GetContourArea);
+            Point[] screenContour = null;
+            foreach (var contour in sortedContours)
+            {
+                var epsilon = 0.02 * Cv2.ArcLength(contour, true);
+                var approx = Cv2.ApproxPolyDP(contour, epsilon, true);
+                if (approx.Length == 4)
+                {
+                    screenContour = approx;
+                    break;
+                }
+            }
+
+            Cv2.DrawContours(image, new List<Point[]> { screenContour }, 0, Scalar.Lime, 2);
+            using (new Window("img", image))
+            {
+                Cv2.WaitKey();
+            }
+        }
+
+        private double GetContourArea(Point[] polygon)
+        {
+            double area = 0;
+            for (var i = 0; i < polygon.Length; i++)
+            {
+                var j = (i + 1) % polygon.Length;
+
+                area += polygon[i].X * polygon[j].Y;
+                area -= polygon[i].Y * polygon[j].X;
+            }
+
+            var contourArea = Math.Abs(area / 2);
+            return contourArea;
+        }
+
         private async Task Reply(Activity activity, string text)
         {
             var connector = new ConnectorClient(new Uri(activity.ServiceUrl));
             await connector.Conversations.ReplyToActivityAsync(activity.CreateReply(text.Replace("\n", "\n\n")));
         }
 
-        private IEnumerable<List<string>> Ocr(string picUrl)
+        private IEnumerable<IEnumerable<string>> Ocr(string file)
         {
             var results = new List<List<string>>();
-            var file = Path.Combine(Path.GetTempPath(), picUrl.GetHashCode().ToString());
-            new WebClient().DownloadFile(picUrl, file);
+
             using (var engine = new TesseractEngine(HostingEnvironment.MapPath(@"~/tessdata"), "eng"))
             {
                 engine.SetVariable("tessedit_char_whitelist", "0123456789");
@@ -103,17 +158,24 @@ namespace Bot_Application.Controllers
                         {
                             if (num.Length == 2 && !row.Exists(s => int.Parse(s) > int.Parse(num)))
                             {
-                                // 2 digit and ascending
+                                // 2 digits and ascending
                                 row.Add(num);
                             }
                         }
-                        System.Diagnostics.Debug.WriteLine(iter.GetConfidence(PageIteratorLevel.TextLine));
+                        Debug.WriteLine(iter.GetConfidence(PageIteratorLevel.TextLine));
                         results.Add(row);
                     } while (iter.Next(PageIteratorLevel.TextLine));
                 }
             }
             File.Delete(file);
             return results;
+        }
+
+        private string DownloadFile(string picUrl)
+        {
+            var file = Path.Combine(Path.GetTempPath(), picUrl.GetHashCode().ToString());
+            new WebClient().DownloadFile(picUrl, file);
+            return file;
         }
     }
 }
