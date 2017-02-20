835f40dc196d19153974580aaeecab7dbc19601b
Kiang Kuang <kiangkuang@gmail.com>
2017-02-20 17:33:37 +0800

    fix perspective and clean up image

--- Bot Application/Controllers/MessagesController.cs
+++ Bot Application/Controllers/MessagesController.cs
@@ -13,6 +13,7 @@ using Microsoft.Bot.Connector;
 using OpenCvSharp;
 using OpenCvSharp.CPlusPlus;
 using Tesseract;
+using Rect = OpenCvSharp.CPlusPlus.Rect;
 
 namespace Bot_Application.Controllers
 {
@@ -70,8 +71,8 @@ namespace Bot_Application.Controllers
                 foreach (var attachment in activity.Attachments)
                 {
                     var file = DownloadFile(attachment.ContentUrl);
-                    ProcessImage(file);
-                    var results = Ocr(file);
+                    var processedImage = ProcessImage(file);
+                    var results = Ocr(processedImage);
                     await Reply(activity, string.Join("\n", results.Select(x => string.Join(" ", x)).ToList()));
                 }
             }
@@ -81,39 +82,114 @@ namespace Bot_Application.Controllers
             }
         }
 
-        private void ProcessImage(string file)
+        private string ProcessImage(string file)
         {
             var orig = Cv2.ImRead(file);
-            var ratio = 800.0 / orig.Height;
-            var blur = new Mat();
-            var edged = new Mat();
+            var ratio = 1000f / Math.Max(orig.Height, orig.Width);
             var image = orig.Clone();
 
-            image = image.Resize(new Size(), ratio, ratio);
-            Cv2.GaussianBlur(image, blur, new Size(5, 5), 0);
-            Cv2.Canny(blur, edged, 75, 200);
+            var edged = CannyEdge(image, ratio);
+            var screenContour = GetContour(edged);
 
+            var boundRect = Cv2.BoundingRect(screenContour);
+            var transformed = FixPerspective(orig, screenContour, boundRect, ratio);
+
+            var cropped = transformed[ResizeBoundRect(boundRect, ratio)];
+            var final = CleanImage(cropped);
+
+            /*
+            using (new Window("img", final))
+            {
+                Cv2.WaitKey();
+            }
+            */
+
+            // File.Delete(file);
+            var newFile = file + "-final.jpg";
+            Cv2.ImWrite(newFile, final);
+
+            return newFile;
+        }
+
+        private Mat FixPerspective(Mat orig, Point[] screenContour, Rect boundRect, float ratio)
+        {
+            var transMtx = GetTransMtx(screenContour, boundRect, ratio);
+            var transformed = new Mat();
+            Cv2.WarpPerspective(orig, transformed, transMtx, orig.Size());
+            return transformed;
+        }
+
+        private static Mat CleanImage(Mat cropped)
+        {
+            var grey = cropped.CvtColor(ColorConversion.RgbToGray);
+            return grey.Threshold(127, 255, ThresholdType.Binary);
+        }
+
+        private Mat GetTransMtx(IList<Point> screenContour, Rect boundRect, float ratio)
+        {
+            var src = RatioPoints(ratio,
+                screenContour[0].X, screenContour[0].Y,
+                screenContour[1].X, screenContour[1].Y,
+                screenContour[2].X, screenContour[2].Y,
+                screenContour[3].X, screenContour[3].Y);
+            var dest = RatioPoints(ratio,
+                boundRect.Right, boundRect.Top,
+                boundRect.Left, boundRect.Top,
+                boundRect.Left, boundRect.Bottom,
+                boundRect.Right, boundRect.Bottom);
+
+            return Cv2.GetPerspectiveTransform(src, dest);
+        }
+
+        private Point[] GetContour(Mat edged)
+        {
             Point[][] contours;
             HierarchyIndex[] hierarchyIndexes;
             Cv2.FindContours(edged.Clone(), out contours, out hierarchyIndexes, ContourRetrieval.List, ContourChain.ApproxSimple);
             var sortedContours = contours.OrderByDescending(GetContourArea);
-            Point[] screenContour = null;
+
+            Point[] selectedContour = null;
             foreach (var contour in sortedContours)
             {
                 var epsilon = 0.02 * Cv2.ArcLength(contour, true);
                 var approx = Cv2.ApproxPolyDP(contour, epsilon, true);
                 if (approx.Length == 4)
                 {
-                    screenContour = approx;
+                    selectedContour = approx;
                     break;
                 }
             }
+            return selectedContour;
+        }
 
-            Cv2.DrawContours(image, new List<Point[]> { screenContour }, 0, Scalar.Lime, 2);
-            using (new Window("img", image))
+        private Mat CannyEdge(Mat image, float ratio)
+        {
+            var blur = new Mat();
+            var edge = new Mat();
+            image = image.Resize(new Size(), ratio, ratio);
+            Cv2.GaussianBlur(image, blur, new Size(5, 5), 0);
+            Cv2.Canny(blur, edge, 75, 200);
+            return edge;
+        }
+
+        private Rect ResizeBoundRect(Rect boundRect, float ratio)
+        {
+            return new Rect(
+                (int)(boundRect.X / ratio),
+                (int)(boundRect.Y / ratio),
+                (int)(boundRect.Width / ratio),
+                (int)(boundRect.Height / ratio));
+        }
+
+        private IEnumerable<Point2f> RatioPoints(float ratio, int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4)
+        {
+            return new List<Point2f>
             {
-                Cv2.WaitKey();
-            }
+                new Point2f(x1/ratio, y1/ratio),
+                new Point2f(x2/ratio, y2/ratio),
+                new Point2f(x3/ratio, y3/ratio),
+                new Point2f(x4/ratio, y4/ratio)
+            };
         }
 
         private double GetContourArea(Point[] polygon)
@@ -127,8 +203,7 @@ namespace Bot_Application.Controllers
                 area -= polygon[i].Y * polygon[j].X;
             }
 
-            var contourArea = Math.Abs(area / 2);
-            return contourArea;
+            return Math.Abs(area / 2);
         }
 
         private async Task Reply(Activity activity, string text)
@@ -167,13 +242,14 @@ namespace Bot_Application.Controllers
                     } while (iter.Next(PageIteratorLevel.TextLine));
                 }
             }
-            File.Delete(file);
+
+            // File.Delete(file);
             return results;
         }
 
         private string DownloadFile(string picUrl)
         {
-            var file = Path.Combine(Path.GetTempPath(), picUrl.GetHashCode().ToString());
+            var file = Path.Combine(Path.GetTempPath(), picUrl.GetHashCode() + ".jpg");
             new WebClient().DownloadFile(picUrl, file);
             return file;
         }
